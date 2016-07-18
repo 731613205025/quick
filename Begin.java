05d32ec7ee0da0d175c33f83c7a7cba5798cb797
731613205025 <maniharish95@gmail.com>
2016-07-18 14:12:15 +0530

    Add files via upload

---
+++ Begin.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package begin;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Begin {
+
+    
+    public static void main(String[] args) {
+           int n1,n2,arm,tot=0,a;
+         System.out.println("Enter the number");
+          Scanner sc=new Scanner(System.in);
+          n1=sc.nextInt();
+          a=n1;
+   while(n1!=0)
+        {
+            n2=n1%10;
+            arm=n2*n2*n2;
+            tot+=arm;
+            n1=n1/10;
+            
+        }
+        if(tot==a)
+            System.out.println("armstrong");  
+        else  
+            System.out.println("not an armstrong number");
+    }
+}
+    
+
